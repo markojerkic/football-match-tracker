a68a054b451e4786466fdff8c5fbd202212cfc1b
Marko Jekrić <markojerkic266@gmail.com>
2023-05-03 21:13:56 +0200

    show current team for player

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -3,7 +3,7 @@ import { TextInput, type Option, Select, DateSelector } from "./form-helpers";
 import { SetStoreFunction } from "solid-js/store";
 import { OptionWithImage } from "~/server/country";
 import { PlayerForm } from "~/server/players";
-import { warn } from "console";
+import { A } from "solid-start";
 
 const PerssonIcon = () => (
   <svg
@@ -158,15 +158,38 @@ export const PlayerDetail = (detail: {
   lastName: string;
   imageSlug: string | undefined;
   currentTeam:
-  | {
-    id: string;
-    name: string;
-    imageSlug: string | null;
-  }
-  | undefined;
+    | {
+        id: string;
+        name: string;
+        imageSlug: string | null;
+      }
+    | undefined;
 }) => {
   return (
     <article class="mx-auto flex w-[90%] flex-col justify-center space-y-4 border-2 border-black p-4 md:w-[50%]">
+      {/* FIXME: only admins can see this */}
+      <div class="flex grow justify-end">
+        <A
+          href={`/admin/player/${detail.id}`}
+          class="btn-outline btn-circle btn justify-self-end"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-4 w-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+            />
+          </svg>
+        </A>
+      </div>
+
       <ImageOrDefaultAvater imageSlug={detail.imageSlug} />
       <h3 class="text-center text-3xl font-semibold">{`${detail.firstName} ${detail.lastName}`}</h3>
 
@@ -175,10 +198,37 @@ export const PlayerDetail = (detail: {
       <div>
         <Switch>
           <Match when={detail.currentTeam} keyed>
-            {(currentTeam) => <>{JSON.stringify(currentTeam)}</>}
+            {(currentTeam) => (
+              <CurrentTeam
+                name={currentTeam.name}
+                id={currentTeam.id}
+                imageSlug={currentTeam.imageSlug}
+              />
+            )}
           </Match>
         </Switch>
       </div>
+
+      <span class="divider" />
     </article>
+  );
+};
+
+const CurrentTeam = (team: {
+  name: string;
+  id: string;
+  imageSlug: string | null;
+}) => {
+  return (
+    <div class="flex flex-col space-y-4">
+      <span class="font-thin">Current team</span>
+      <A class="flex items-center space-x-4" href={`/team/${team.id}`}>
+        <img
+          src={team.imageSlug ?? "/shield.svg"}
+          class="avatar h-16 object-cover"
+        />
+        <span class="font-semibold">{team.name}</span>
+      </A>
+    </div>
   );
 };
